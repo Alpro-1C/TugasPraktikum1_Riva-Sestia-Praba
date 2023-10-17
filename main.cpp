4fbdf16745a5f4fb35970ccfd5527bfc9ad5adad
rivaprba <147835771+rivaprba@users.noreply.github.com>
2023-10-17 07:45:40 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{    cout << "-----------------------------------------------------" << endl;
+    cout << "Nama : Riva Sestia Praba\n" << endl;
+    cout << "NPM : 2310631170045\n" << endl;
+    cout << "Kelas : C - Informatika\n" << endl;
+    cout << "Cita - Cita : Web Developer\n" << endl;
+    cout << "Motivasi Masuk Informatika : Dari Guru Informatika SMA" << endl;
+    cout << "------------------------------------------------------" << endl;
+
+        return 0;
+}
